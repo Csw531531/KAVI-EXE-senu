9b64f7b7d9cff1ce66cbb578e8a8b8df9699cb0d
Csw531531 <senuralakshan456@gmail.com>
2025-02-04 00:42:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=Q8o32T6Y#MiZAjyqkHKgHaZYKKOi6iCwmBweIW1uVeuabt4VI3ow",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=yrAnCQBT#yvhBojmMrn4vaYQlsyZbg7DPv0LVv253lTHolp7ql-A",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-ANTI_CALL: process.env.ANTI_CALL || "false",
+ANTI_CALL: process.env.ANTI_CALL || "true",
 };
